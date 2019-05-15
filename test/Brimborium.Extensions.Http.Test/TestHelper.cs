2e306e28b1c8d5036606deb86baba3572111ad79
FlorianGrimm <grimmuko@googlemail.com>
2019-05-15 03:57:59 +0200

    create test scope

---
+++ test/Brimborium.Extensions.Http.Test/TestHelper.cs
@@ -0,0 +1,21 @@
+﻿namespace Brimborium.Extensions.Http.Test {
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+
+    using Microsoft.Extensions.Logging;
+    using Microsoft.Extensions.DependencyInjection;
+
+    public static class TestHelper {
+        public static ServiceProvider CreateServiceProvider(Action<IServiceCollection> addServices) {
+            var services = new Microsoft.Extensions.DependencyInjection.ServiceCollection();
+            services.AddLogging((loggingBuilder) => {
+                loggingBuilder.AddDebug();
+            });
+            if (addServices != null) {
+                addServices(services);
+            }
+            return services.BuildServiceProvider(true);
+        }
+    }
+}
--- test/Brimborium.Extensions.Http.Test/Usage.cs
+++ test/Brimborium.Extensions.Http.Test/Usage.cs
@@ -5,30 +5,34 @@ namespace Brimborium.Extensions.Http.Test {
     public class Usage {
         [Fact]
         public void Usage1() {
-            var sut = new HttpClientGenerator(null, null, null, null);
-            var cfg1 = new HttpClientConfiguration();
-            var cfg2 = new HttpClientConfiguration();
-            var client1 = sut.CreateHttpClient(cfg1);
-            var client2 = sut.CreateHttpClient(cfg2);
-            Assert.NotSame(client1, client2);
+            using (var services = TestHelper.CreateServiceProvider(null)) {
+                var sut = new HttpClientGenerator(null, null, null, null);
+                var cfg1 = new HttpClientConfiguration();
+                var cfg2 = new HttpClientConfiguration();
+                var client1 = sut.CreateHttpClient(cfg1);
+                var client2 = sut.CreateHttpClient(cfg2);
+                Assert.NotSame(client1, client2);
+            }
         }
         [Fact]
         public void Usage2() {
-            var sut = new HttpClientGenerator(null, null, null, null);
-            var cfg1 = new HttpClientConfiguration() { BaseAddress = "https://www.wikipedia.org/" };
-            var cfg2 = new HttpClientConfiguration() { BaseAddress = "https://www.wikipedia.org/" };
+            using (var services = TestHelper.CreateServiceProvider(null)) {
+                var sut = new HttpClientGenerator(null, null, null, null);
+                var cfg1 = new HttpClientConfiguration() { BaseAddress = "https://www.wikipedia.org/" };
+                var cfg2 = new HttpClientConfiguration() { BaseAddress = "https://www.wikipedia.org/" };
 
-            var client1 = sut.CreateHttpClient(cfg1);
-            var client2 = sut.CreateHttpClient(cfg2);
-            //Assert.Same(client1, client2);
+                var client1 = sut.CreateHttpClient(cfg1);
+                var client2 = sut.CreateHttpClient(cfg2);
+                //Assert.Same(client1, client2);
 
-            cfg2.BaseAddress = "https://localhost";
-            var client3 = sut.CreateHttpClient(cfg2);
-            //Assert.NotSame(client2, client3);
+                cfg2.BaseAddress = "https://localhost";
+                var client3 = sut.CreateHttpClient(cfg2);
+                //Assert.NotSame(client2, client3);
 
-            client1.Dispose();
-            client2.Dispose();
-            client3.Dispose();
+                client1.Dispose();
+                client2.Dispose();
+                client3.Dispose();
+            }
         }
     }
 }
