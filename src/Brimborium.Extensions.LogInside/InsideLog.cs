b88ee09fb9b72109c69d5dc0789c137cddb1e736
FlorianGrimm <grimmuko@googlemail.com>
2019-06-19 05:28:54 +0200

    search for an good solution

--- src/Brimborium.Extensions.LogInside/InsideLog.cs
+++ src/Brimborium.Extensions.LogInside/InsideLog.cs
@@ -2,5 +2,41 @@
 
 namespace Brimborium.Extensions.LogInside {
     public class InsideLog {
+        private InsideLog _InsideLog;
+        private string _Message;
+
+        public InsideLog() {
+        }
+
+        public InsideLog(string message) {
+            this._Message = message;
+        }
+
+        public InsideLog(InsideLog previous, string message) {
+            this._InsideLog = previous;
+            this._Message = message;
+        }
+
+        //public bool Condition(bool value, string message) {
+        //    return value;
+        //}
+        //public T Value<T>(T value, string message) {
+        //    return value;
+        //}
+
+        //public bool Condition<T>(T value, Func<T, bool> func, string message) {
+        //    throw new NotImplementedException();
+        //}
+
+        //public T Value<T, R>(T value, Func<T, R> func, string message) {
+        //    throw new NotImplementedException();
+        //}
+        public InsideLog Log(string message) {
+            return new InsideLog(this, message) ;
+        }
+
+        public override string ToString() {
+            return this._Message;
+        }
     }
 }
--- test/Brimborium.Extensions.LogInside.Test/Usage.cs
+++ test/Brimborium.Extensions.LogInside.Test/Usage.cs
@@ -3,9 +3,50 @@ using Xunit;
 
 namespace Brimborium.Extensions.LogInside.Test {
     public class Usage {
+        private string Message;
+
+        private void Log(string message) {
+            this.Message = message;
+        }
+
         [Fact]
         public void Usage1() {
+
             var x = new InsideLog();
+            for (int i = 0; i < 10; i++) {
+                var y = x.Log(i.ToString());
+                try {
+                    if (i == 4) {
+                        throw new Exception("four");
+                    }
+                } catch (Exception error){
+                    this.Log($"{error.Message} {y}");
+                }
+            }
+            Assert.Equal("four 4", this.Message);
+            //var value1 = getRandom();
+            //var value2 = getRandom();
+            //var condition1 = x.Condition(
+            //    ((value1 % 2) == 0), 
+            //    "condition1");
+            //var condition2 = x.Condition(
+            //    (value1, value2),
+            //    (a) => (((a.value1+a.value2) % 2) == 0),
+            //    "condition2");
+            //try {
+            //    if (condition1) {
+            //        throw new Exception("T");
+            //    } else {
+            //        throw new Exception("F");
+            //    }
+            //} catch {
+            //}
+            //if (condition2) {
+            //}
+        }
+
+        private int getRandom() {
+            return (new Random()).Next();
         }
     }
 }
