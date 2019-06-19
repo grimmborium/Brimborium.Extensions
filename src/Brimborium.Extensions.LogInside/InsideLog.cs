4141423fc9183f03a010480f14c6c1b5b3ea9ea2
FlorianGrimm <grimmuko@googlemail.com>
2019-06-19 05:06:09 +0200

    LogInside

---
+++ src/Brimborium.Extensions.LogInside/InsideLog.cs
@@ -0,0 +1,6 @@
+﻿using System;
+
+namespace Brimborium.Extensions.LogInside {
+    public class InsideLog {
+    }
+}
---
+++ test/Brimborium.Extensions.LogInside.Test/Usage.cs
@@ -0,0 +1,11 @@
+using System;
+using Xunit;
+
+namespace Brimborium.Extensions.LogInside.Test {
+    public class Usage {
+        [Fact]
+        public void Usage1() {
+            var x = new InsideLog();
+        }
+    }
+}
