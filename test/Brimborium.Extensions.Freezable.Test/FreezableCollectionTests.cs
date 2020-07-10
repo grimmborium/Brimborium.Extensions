3050bdfc002abd30565ddfe711924bb0faab855c
FlorianGrimm <grimmuko@googlemail.com>
2020-07-10 02:53:29 +0200

    test

--- test/Brimborium.Extensions.Freezable.Test/FreezableCollectionTests.cs
+++ test/Brimborium.Extensions.Freezable.Test/FreezableCollectionTests.cs
@@ -1,4 +1,5 @@
-﻿using Xunit;
+﻿#pragma warning disable xUnit2013 // Do not use equality check to check for collection size.
+using Xunit;
 using Brimborium.Extensions.Freezable;
 
 using System;
@@ -9,81 +10,13 @@ namespace Brimborium.Extensions.Freezable {
     public class FreezableCollectionTests {
         [Fact()]
         public void FreezableCollectionTest() {
-            Assert.True(false, "This test needs an implementation");
+            var sut = new FreezableCollection<int>();
+            Assert.Equal(0, sut.Count);
+            sut.Add(1);
+            Assert.Equal(1, sut.Count);
+            sut.Add(2);
+            Assert.Equal(2, sut.Count);
         }
 
-        [Fact()]
-        public void FreezableCollectionTest1() {
-            Assert.True(false, "This test needs an implementation");
-        }
-
-        [Fact()]
-        public void AddTest() {
-            Assert.True(false, "This test needs an implementation");
-        }
-
-        public void AddRangeTest() {
-            Assert.True(false, "This test needs an implementation");
-        }
-
-        [Fact()]
-        public void ClearTest() {
-            Assert.True(false, "This test needs an implementation");
-        }
-
-        [Fact()]
-        public void ContainsTest() {
-            Assert.True(false, "This test needs an implementation");
-        }
-
-        [Fact()]
-        public void CopyToTest() {
-            Assert.True(false, "This test needs an implementation");
-        }
-
-        [Fact()]
-        public void GetEnumeratorTest() {
-            Assert.True(false, "This test needs an implementation");
-        }
-
-        [Fact()]
-        public void IndexOfTest() {
-            Assert.True(false, "This test needs an implementation");
-        }
-
-        [Fact()]
-        public void InsertTest() {
-            Assert.True(false, "This test needs an implementation");
-        }
-
-        [Fact()]
-        public void RemoveTest() {
-            Assert.True(false, "This test needs an implementation");
-        }
-
-        [Fact()]
-        public void RemoveAtTest() {
-            Assert.True(false, "This test needs an implementation");
-        }
-
-        [Fact()]
-        public void FreezeTest() {
-            Assert.True(false, "This test needs an implementation");
-        }
-
-        [Fact()]
-        public void IsFrozenTest() {
-            Assert.True(false, "This test needs an implementation");
-        }
-
-        [Fact()]
-        public void CopyValueTest() {
-            Assert.True(false, "This test needs an implementation");
-        }
-
-        [Fact()]
-        public void ToListTest() {
-            Assert.True(false, "This test needs an implementation");
-        }
     }
 }
--- test/Brimborium.Extensions.Freezable.Test/FreezableDictionaryTests.cs
+++ test/Brimborium.Extensions.Freezable.Test/FreezableDictionaryTests.cs
@@ -1,4 +1,5 @@
-﻿using Xunit;
+﻿#pragma warning disable xUnit2013 // Do not use equality check to check for collection size.
+using Xunit;
 using Brimborium.Extensions.Freezable;
 
 using System;
@@ -9,107 +10,13 @@ namespace Brimborium.Extensions.Freezable {
     public class FreezableDictionaryTests {
         [Fact()]
         public void FreezableDictionaryTest() {
-            Assert.True(false, "This test needs an implementation");
+            var sut = new FreezableDictionary<int, int>();
+            Assert.Equal(0, sut.Count);
+            sut.Add(1, 1);
+            sut.Add(2, 2);
+            Assert.Equal(2, sut.Count);
+            sut.Freeze();
+            Assert.Throws<System.InvalidOperationException>(()=> sut.Add(3, 3));
         }
-
-        [Fact()]
-        public void FreezableDictionaryTest1() {
-            Assert.True(false, "This test needs an implementation");
-        }
-
-        [Fact()]
-        public void FreezableDictionaryTest2() {
-            Assert.True(false, "This test needs an implementation");
-        }
-
-        [Fact()]
-        public void AddRangeTest() {
-            Assert.True(false, "This test needs an implementation");
-        }
-
-        [Fact()]
-        public void AddTest() {
-            Assert.True(false, "This test needs an implementation");
-        }
-
-        [Fact()]
-        public void AddTest1() {
-            Assert.True(false, "This test needs an implementation");
-        }
-
-        [Fact()]
-        public void ClearTest() {
-            Assert.True(false, "This test needs an implementation");
-        }
-
-        [Fact()]
-        public void ContainsTest() {
-            Assert.True(false, "This test needs an implementation");
-        }
-
-        [Fact()]
-        public void ContainsKeyTest() {
-            Assert.True(false, "This test needs an implementation");
-        }
-
-        [Fact()]
-        public void CopyToTest() {
-            Assert.True(false, "This test needs an implementation");
-        }
-
-        [Fact()]
-        public void GetEnumeratorTest() {
-            Assert.True(false, "This test needs an implementation");
-        }
-
-        [Fact()]
-        public void RemoveTest() {
-            Assert.True(false, "This test needs an implementation");
-        }
-
-        [Fact()]
-        public void RemoveTest1() {
-            Assert.True(false, "This test needs an implementation");
-        }
-
-        [Fact()]
-        public void TryGetValueTest() {
-            Assert.True(false, "This test needs an implementation");
-        }
-
-        [Fact()]
-        public void FreezeTest() {
-            Assert.True(false, "This test needs an implementation");
-        }
-
-        [Fact()]
-        public void IsFrozenTest() {
-            Assert.True(false, "This test needs an implementation");
-        }
-
-        [Fact()]
-        public void CloneTest() {
-            Assert.True(false, "This test needs an implementation");
-        }
-
-        [Fact()]
-        public void CopyValueTest() {
-            Assert.True(false, "This test needs an implementation");
-        }
-
-        [Fact()]
-        public void CopyValueTest1() {
-            Assert.True(false, "This test needs an implementation");
-        }
-
-        [Fact()]
-        public void CopyKeyValueTest() {
-            Assert.True(false, "This test needs an implementation");
-        }
-
-        [Fact()]
-        public void CopyKeyValueTest1() {
-            Assert.True(false, "This test needs an implementation");
-        }
-    }
+   }
 }
